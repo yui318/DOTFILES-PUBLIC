24d3034d30cc6077c6014d0da20602ca920e77ac
水上 陽日 <133027@design-network.local>
2026-05-14 14:54:01 +0900

    update: emacs

--- .emacs.d/NOTE.md
+++ .emacs.d/NOTE.md
@@ -1,7 +1,7 @@
 ## References
 
 - [2020年代のEmacs入門](https://emacs-jp.github.io/tips/emacs-in-2020)
-- []()
+- [Leaf DOCS](https://elpa.gnu.org/packages/leaf.html)
 
 
 
@@ -12,4 +12,25 @@ M-: user-init-fileでinit.elの位置を確認できる
 ```powershell
 $REPOSITORY_PATH = "$HOME\dotfiles" # replace your path
 New-Item -ItemType SymbolicLink -Path "$env:APPDATA\.emacs.d" -Target "$REPOSITORY_PATH\.emacs.d"
+```
+
+
+# AVY
+vscodeにもJumpyやAce Jumpというものがあるらしいがこの記事が面白かった。
+https://emacs-jp.github.io/tips/avy-can-do-anything
+自分もこういう記事書きたい
+
+```el
+(leaf avy
+  :ensure t
+  :bind
+  (("C-:" . avy-goto-char-timer)
+   ;;("C-*" . avy-resume)
+   ;; ("M-g M-g" . avy-goto-line)
+   )
+  :config
+  (leaf avy-zap
+    :ensure t
+    :bind
+    ([remap zap-to-char] . avy-zap-to-char)))
 ```
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -138,8 +138,9 @@
   :ensure t
   :bind
   (("C-:" . avy-goto-char-timer)
-   ("C-*" . avy-resume)
-   ("M-g M-g" . avy-goto-line))
+   ;;("C-*" . avy-resume)
+   ;; ("M-g M-g" . avy-goto-line)
+   )
   :config
   (leaf avy-zap
     :ensure t
@@ -152,6 +153,18 @@
 
 (provide 'init)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 ;; Local Variables:
 ;; indent-tabs-mode: nil
 ;; End:
